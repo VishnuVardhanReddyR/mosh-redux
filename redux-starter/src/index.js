f7f94c0e14e59cb8d2d056c564ef1cc52c4d54c3
R Vishnu Vardhan Reddy <vishnurapuru10@gmail.com>
2023-01-18 16:32:12 -0500

    middleware

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,23 +1,5 @@
-import store from "./store";
+import configureStore from "./store/configureStore";
 
-const unsubscribe = store.subscribe(() => {
-    console.log("Store Changed!", store.getState());
-});
+const store = configureStore();
 
-store.dispatch({
-    type: "bugAdded",
-    payload: {
-        description: "Bug1"
-    }
-});
-
-unsubscribe();
-
-store.dispatch({
-    type: "bugRemoved",
-    payload: {
-        id: 1
-    }
-});
-
-console.log(store.getState());
+store.dispatch({});
---
+++ redux-starter/src/store/actions.js
@@ -0,0 +1,14 @@
+
+export const bugAdded = (description) => ({
+    type: actions.BUG_ADDED,
+    payload: {
+        description,
+    }
+});
+
+export const bugResolved = id => ({
+    type: actions.BUG_RESOLVED,
+    payload: {
+        id
+    }
+});
